a67228362b4d652d4ee9e03a6901c474f1aa7bd6
smertin123 <smertin@gmail.com>
2023-06-05 21:42:20 +0800

    store results in file without duplicates

--- subdomain-enumerator.py
+++ subdomain-enumerator.py
@@ -15,47 +15,60 @@ from config import API_KEY
 # Ask the user for the target domain
 target = input("Enter target: ")
 
-def certUtil(target):
-    #print message to console
-    print("Searching for subdomains with crt.sh...")
-    # Combine the target with the URL query parameter
-    url = "https://crt.sh/?q=" + target
-    # Send the request
-    html_content = requests.get(url).text
-    # Parse the request
-    soup = BeautifulSoup(html_content, "lxml")
-    # Find all instances within data containing the target domain
-    subdomains = soup.find_all(string=re.compile(target))
-    print("Identified subdomains for " + target + ":\n")
-    # Print each finding to the CLI
-    for i in subdomains: 
-        print(i.string)
+filename = target + '.txt'
+
+def crtsh(target):
+    with open(filename, 'a+') as file:
+        file.seek(0)
+        existing_subdomains = file.read().splitlines()
+
+        print("Searching for subdomains with crt.sh...")
+        url = "https://crt.sh/?q=" + target
+        html_content = requests.get(url).text
+        soup = BeautifulSoup(html_content, "lxml")
+        subdomains = soup.find_all(string=re.compile("." + target))
+        for i in subdomains:
+            subdomain = i
+            if subdomain not in existing_subdomains:
+                file.write(subdomain + '\n')
+                existing_subdomains.append(subdomain)                
+        print("crt.sh results stored in " + filename)
+        print("")
 
 def urlScan(target):
-    #print message to console
-    print("Searching for subdomains with urlscan.io...")
-    headers = {'API-Key':API_KEY,'Content-Type':'application/json'}
-    data = {"url": target, "visibility": "public"}
-    print("Generating unique UUID...")
-    #send an api call to generate a request ID
-    response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
-    response_list = response.json()
-    #grab the unique request UUID search url
-    url = response_list["api"]
-    #urlscan takes a while to populate data. sleep for 2 minutes.
-    print("Urlscan takes a while to populate...Waiting for 1 minute")
-    time.sleep(30)
-    print("Just 30 seconds more...")
-    time.sleep(30)
-    #send a request to the unique UUID search url
-    response = requests.get(url)
-    response_list = response.json()
-    #grab the subdomains from the response
-    subdomains = response_list["data"]["requests"][0]["request"]["redirectResponse"]["securityDetails"]["sanList"]
-    # Print each finding to the CLI
-    for i in subdomains: 
-        print(i)
+    with open(filename, 'a+') as file:
+        file.seek(0)  # Move the file cursor to the beginning
+        existing_subdomains = file.read().splitlines()  # Read the file contents into a list
+        #print message to console
+        print("Searching for subdomains with urlscan.io...")
+        headers = {'API-Key': API_KEY, 'Content-Type': 'application/json'}
+        data = {"url": target, "visibility": "public"}
+        print("Generating unique UUID...")
+        #send an api call to generate a request ID
+        response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
+        response_list = response.json()
+        #grab the unique request UUID search url
+        url = response_list["api"]
+        #urlscan takes a while to populate data. sleep for 2 minutes.
+        print("Urlscan takes a while to populate... Waiting for 1 minute")
+        time.sleep(30)
+        print("Just 30 seconds more...")
+        time.sleep(30)
+        #send a request to the unique UUID search url
+        response = requests.get(url)
+        response_list = response.json()
+        #grab the subdomains from the response
+        subdomains = response_list["data"]["requests"][0]["request"]["redirectResponse"]["securityDetails"]["sanList"]
+        print("Identified subdomains for " + target + ":\n")
+        # Print each finding to the CLI
+        for i in subdomains:
+            subdomain = i
+            if subdomain.endswith(target) and subdomain not in existing_subdomains:
+                file.write(subdomain + '\n')
+                existing_subdomains.append(subdomain)  
+        print("Results stored in " + filename)
+        print("")
 
 #pass the target to all functions
-certUtil(target)
+crtsh(target)
 urlScan(target)

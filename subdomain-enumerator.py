1a3d68735195a5f8bb6f20c7f8c6d8e0d16ad19b
smertin123 <smertin@gmail.com>
2023-06-06 12:37:46 +0800

    added threat crowd scan

--- subdomain-enumerator.py
+++ subdomain-enumerator.py
@@ -17,26 +17,41 @@ def crtsh(target, filename):
     with open(filename, 'a+') as file:
         file.seek(0)
         existing_subdomains = file.read().splitlines()
-
         print("Searching for subdomains with crt.sh...")
         url = "https://crt.sh/?q=" + target
         html_content = requests.get(url).text
         soup = BeautifulSoup(html_content, "lxml")
         subdomains = soup.find_all(string=re.compile(target))
         uniqueCount = 0
-        for i in subdomains:
-            subdomain = i
+        for subdomain in subdomains:
             if subdomain not in existing_subdomains:
                 file.write(subdomain + '\n')
                 existing_subdomains.append(subdomain)
                 uniqueCount += 1
         print(str(uniqueCount) + " unique subdomains stored in " + filename + "\n")
-
+        
+def threatCrowd(target, filename):
+    with open(filename, 'a+') as file:
+        file.seek(0)
+        existing_subdomains = file.read().splitlines()
+        print("Searching for subdomains with threat crowd...")
+        url = "http://ci-www.threatcrowd.org/domain.php?domain=" + target
+        html_content = requests.get(url).text
+        soup = BeautifulSoup(html_content, "lxml")
+        html_string = soup.prettify()
+        subdomains = re.findall(r'<a href="/domain\.php\?domain=([^"]+\.target\.com)">', html_string)
+        uniqueCount = 0
+        for subdomain in subdomains:
+            if subdomain not in existing_subdomains:
+                file.write(subdomain + '\n')
+                existing_subdomains.append(subdomain)
+                uniqueCount += 1
+        print(str(uniqueCount) + " unique subdomains stored in " + filename + "\n")
+        
 def urlScan(target, filename):
     with open(filename, 'a+') as file:
         file.seek(0)  # Move the file cursor to the beginning
         existing_subdomains = file.read().splitlines()  # Read the file contents into a list
-
         def search_json(json_obj, target, file, existing_subdomains):
             uniqueCount = 0
             if isinstance(json_obj, dict):
@@ -54,30 +69,25 @@ def urlScan(target, filename):
                 for item in json_obj:
                     uniqueCount += search_json(item, target, file, existing_subdomains)
             return uniqueCount
-
         print("Searching for subdomains with urlscan.io...")
         headers = {'API-Key': API_KEY, 'Content-Type': 'application/json'}
         data = {"url": target, "visibility": "public"}
-
         print("Generating unique UUID...")
         response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
         response_list = response.json()
         url = response_list["api"]
-
         print("Urlscan takes a while to populate... Waiting for 30 seconds")
         time.sleep(30)
-
         response = requests.get(url)
         data = response.json()
-
         # Call the search_json function with existing_subdomains argument
         unique_subdomain_count = search_json(data, target, file, existing_subdomains)
-
         print(str(unique_subdomain_count) + " unique subdomains stored in " + filename + "\n")
 
 #pass the target to all functions
 def run(target, filename):
     crtsh(target, filename)
+    threatCrowd(target, filename)
     urlScan(target, filename)
 
 try:
@@ -85,17 +95,15 @@ try:
         print("\nUsage: subdomain-enumerator.py [OPTION] [TARGET]\n")
         print("-s, --scan         scan target for subdomains (results stored in [TARGET].txt)")
         print("-h, --help         display this help menu\n")
-        
     if len(sys.argv) < 2 or (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
         help()
-
     if len(sys.argv) > 2:
         target = sys.argv[-1]
         filename = str(target) + '.txt'
         if (sys.argv[1] == "--scan" or sys.argv[1] == "-s"):
             run(target, filename)
         else:
-            print("Invalid option")
+            print("Invalid option")   
             
 except KeyboardInterrupt:
     print("\nTerminated by user.")
